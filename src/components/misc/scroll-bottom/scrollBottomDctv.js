bdd491a02faa28c143d71fd11657f98a80223066
Toks Fifo <toksfifo@gmail.com>
2015-10-28 19:33:21 -0400

    transition on form

--- src/components/misc/scroll-bottom/scrollBottomDctv.js
+++ src/components/misc/scroll-bottom/scrollBottomDctv.js
@@ -2,10 +2,10 @@ angular.module('quote')
 	.directive('scrollBottomDctv', scrollBottomDctv);
 
 /**
- * Scroll to bottom of self when element is added to array
+ * Scroll (and transition) to bottom of self when element is added to array
  * e.g. <div scroll-bar-dctv="arrayToWatch"></div>
  */
-function scrollBottomDctv() {
+function scrollBottomDctv($timeout, $interval) {
 
 	return {
 		restrict: 'A',
@@ -22,9 +22,24 @@ function scrollBottomDctv() {
 			return scope.scrollBottomDctv.length;
 		}, function(newVal, oldVal) {
 			if (oldVal && (newVal > oldVal)) {
-				elem[0].scrollTop = elem[0].scrollHeight;
+				var offset = elem[0].scrollHeight - elem[0].offsetHeight;
+
+				// timeout to wait in case there's an ng-repeat enter transition on the new element
+				$timeout(function() {
+					scroll(elem[0], offset, 100);
+				}, 250);
 			}
 		});
+	}
+
+	function scroll(elem, to, duration) {
+		var timestep = 20;
+		var distancestep = (to - elem.scrollTop) / (duration / timestep);
+
+		var move = $interval(function() {
+			elem.scrollTop += distancestep;
+			if (elem.scrollTop >= to) $interval.cancel(move);
+		}, timestep);
 	}
 
 }
